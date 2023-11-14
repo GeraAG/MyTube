cffa5ea578ea92fa6855cf9c4db08102ece5cbdc
GeraAG <ag.andrew.gerasimenko@gmail.com>
2023-11-14 19:02:49 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from pytube import YouTube
+
+# URL of the video
+url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+
+# Create a YouTube object
+yt = YouTube(url)
+
+# Get the highest resolution progressive stream
+stream = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first()
+
+# Download the stream
+stream.download()
